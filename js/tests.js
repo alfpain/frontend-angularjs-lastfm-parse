0e4756bf33416fcb8d528e710561c798485d29bd
brandon <josueknd@hotmail.es>
2015-01-30 10:54:56 -0600

    cambios

--- js/tests.js
+++ js/tests.js
@@ -1,20 +1,23 @@
-describe('PasswordController', function() {
-  beforeEach(module('app'));
-
+describe("Testing", function(){
+  beforeEach(module('AuthApp'));
   var $controller;
-
-  beforeEach(inject(function(_$controller_){
+   beforeEach(inject(function(_$controller_){
     // The injector unwraps the underscores (_) from around the parameter names when matching
     $controller = _$controller_;
   }));
 
-  describe('$scope.grade', function() {
-    it('sets the strength to "strong" if the password length is >8 chars', function() {
-      var $scope = {};
-      var controller = $controller('PasswordController', { $scope: $scope });
-      $scope.password = 'longerthaneightchars';
-      $scope.grade();
-      expect($scope.strength).toEqual('strong');
-    });
+  it("si es verdadero", function(){
+    expect(user).not.toBe("");
+
   });
+  it("asdsa"), function(){
+
+     expect(user).toEqual(0);
+     
+  }
+
+  it("prueba"), function(){
+
+    expect($('<input type="text" />').focus()).toBeFocused()
+  }
 });
